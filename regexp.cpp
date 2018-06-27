95c506ca466cec3ff8a8c5a529cb36983a011574
wqlei <40601230+wqlei@users.noreply.github.com>
2018-06-27 22:29:29 +0800

    Update regexp.cpp

--- regexp.cpp
+++ regexp.cpp
@@ -4,14 +4,16 @@
 
 using namespace std;
 
-int main()
+int regexp()
 {
 	boost::regex reg("tel:[\\*#+\\d]+");
 	string correct = "tel:+86252277";
 	string incorrect = "sip:+86252277@zte.com.cn";
-	if (boost::regex_match(correct,reg))
+	boost::cmatch res;
+
+	if (boost::regex_match(correct.c_str(),res,reg))
 	{
-		cout << "correct match"<< endl;
+		cout << "correct match:"<< res[0]<<endl;
 	}
 	else
 	{
@@ -26,4 +28,20 @@ int main()
 	{
 		cout << "incorrect not match" << endl;
 	}
+
+	/*const char* mail = "tengxun@qq.com";
+	boost::cmatch res;
+	//建立3个子表达式
+	boost::regex reg("(\\w+)@(\\w+).(\\w+)");
+	if (boost::regex_match(mail, res, reg))
+	{
+		//既可以通过迭代器获取数据， 也可以通过数组方式获取数据
+		for (boost::cmatch::iterator pos = res.begin(); pos != res.end(); ++pos)
+		{
+			std::cout << *pos << std::endl;
+		}
+		//res[0]存放匹配到的完整字符串
+		std::cout << "name:" << res[1] << std::endl;
+	}*/
+	return 0;
 }
