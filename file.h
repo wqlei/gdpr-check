ff4c2e3aab196e111fe3057590fc78126462f646
wqlei <40601230+wqlei@users.noreply.github.com>
2018-06-30 09:02:15 +0800

    Create file.h

---
+++ file.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<string>
+//#include<vector>
+#include <boost/filesystem.hpp>
+
+using namespace boost::filesystem;
+using namespace std;
+
+vector<string> SrcFiles;
+int Files_Analys(const char *Input_Exp_Dir, const char *Input_Src_Dir);
