55ec74f281282da0119c2f9b253bec4eaae79620
wqlei <40601230+wqlei@users.noreply.github.com>
2018-07-04 23:42:59 +0800

    Create outcsv.h

---
+++ outcsv.h
@@ -0,0 +1,10 @@
+#ifndef _OUTCSV
+
+#define _OUTCSV
+
+#include<iostream>
+
+void Creat_Out_Csv();
+void Output_Ret_To_Csv(std::string File_Name, int line, const char* ret, std::string Rd_Buf);
+
+#endif
