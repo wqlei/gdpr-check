f040b511620383416f6caf7a18c360c9c0c1a0ac
wqlei <40601230+wqlei@users.noreply.github.com>
2018-07-04 23:31:39 +0800

    Update compare.cpp

--- compare.cpp
+++ compare.cpp
@@ -1,56 +1,33 @@
 #include <iostream>
 #include <fstream>
-#include<string>
-
+#include<boost/regex.hpp>
+#include"compare.h"
 using namespace std;
 
-int Judge_Cmpfile_Is_Same(string cmpfile, int cmpline,string checkbuffer)
+extern map<string, string> Pre_Conf;
+
+int Judge_Cmpfile_Is_Same(string File_Name, int line, string ret)
 {
-	int Ret = 0;
 	string TmpBuffer;
-	ifstream CmpFile(cmpfile);
+	string expstr = (Pre_Conf["5_export_directory"]);
+	//cout << "Judge_Cmpfile_Is_Same" << exp << endl;
+	string src;
+	string repstr = Pre_Conf["4_src_directory"];
+	src = File_Name.replace(File_Name.find(Pre_Conf["5_export_directory"]), strlen(expstr.c_str()), repstr);
+	//src = boost::regex_replace(File_Name, reg, rep);
+	//cout << "Judge_Cmpfile_Is_Same" << src << endl;
+	
+	ifstream CmpFile;
+	CmpFile.open(src,ios::in);
 	if (!CmpFile.is_open())
 	{
-		cout << "Open CmpFile Fail" << endl;
+		cout << "打开文件："<<src<<"出错" << endl;
 	}
-	while (!CmpFile.eof() && cmpline)
+	while (!CmpFile.eof() && line)
 	{
 		getline(CmpFile, TmpBuffer);
-		cmpline--;
+		line--;
 	}
-	Ret = strcmp(checkbuffer.c_str(), TmpBuffer.c_str());
-	return Ret;
-}
-
-int cmp()
-{
-	string filename1, filename2;
-	string buffer;
-	int linenum = 1;
-	bool need_cmp = false;
-	cerr << "Input FileNane:" << endl;
-	cin >> filename1 >> filename2;
-	ifstream checkfile(filename1);
-
-	if (!checkfile.is_open())
-	{
-		cout << "Open File Fail" << endl;
-	}
-
-	while (!checkfile.eof())
-	{
-		getline(checkfile, buffer);
-
-		if (0 == strcmp(buffer.c_str(), "hjkl"))
-			need_cmp = true;
-
-		if (need_cmp)
-		{
-			if (0 != Judge_Cmpfile_Is_Same(filename2, linenum, buffer))
-			cout << buffer << "\t" << linenum << endl;
-			break;
-		}
-		linenum++;
-	}
-	return 0;
+	//cout << "Judge_Cmpfile_Is_Same" << "\t" <<ret << "\t"<<TmpBuffer << endl;
+	return strcmp(ret.c_str(), TmpBuffer.c_str());
 }
