b7101de431157ba62dee2742e57bf9c91daec73b
wqlei <40601230+wqlei@users.noreply.github.com>
2018-07-04 23:34:07 +0800

    Update regexp.cpp

--- regexp.cpp
+++ regexp.cpp
@@ -1,47 +1,92 @@
-#include<iostream>
-#include<boost/regex.hpp>
-#include<string>
+#include"regexp.h"
+#include"compare.h"
+#include"outcsv.h"
+#include<fstream>
 
 using namespace std;
+extern map<std::string, std::string> Pre_Conf;
 
-int regexp()
+int Get_Conf(map<string, string> &config)
 {
-	boost::regex reg("tel:[\\*#+\\d]+");
-	string correct = "tel:+86252277";
-	string incorrect = "sip:+86252277@zte.com.cn";
-	boost::cmatch res;
-
-	if (boost::regex_match(correct.c_str(),res,reg))
+	ifstream Conf_File;
+	boost::regex reg("^[\\s]*([^:#]*)[:][\\s]*(.*)$");
+	boost::regex::icase;
+	Conf_File.open("check.conf", ios::in);
+	if (!Conf_File.is_open())
 	{
-		cout << "correct match:"<< res[0]<<endl;
+		return -1;
 	}
-	else
+	while (!Conf_File.eof())
 	{
-		cout << "correct not match"<< endl;
-	}
-
-	if (boost::regex_match(incorrect, reg))
-	{
-		cout << "incorrect match" << endl;
-	}
-	else
-	{
-		cout << "incorrect not match" << endl;
-	}
-
-	/*const char* mail = "tengxun@qq.com";
-	boost::cmatch res;
-	//建立3个子表达式
-	boost::regex reg("(\\w+)@(\\w+).(\\w+)");
-	if (boost::regex_match(mail, res, reg))
-	{
-		//既可以通过迭代器获取数据， 也可以通过数组方式获取数据
-		for (boost::cmatch::iterator pos = res.begin(); pos != res.end(); ++pos)
+		string conf_line;
+		boost::cmatch ret;
+		getline(Conf_File, conf_line);
+		if (boost::regex_match(conf_line.c_str(), ret, reg))
 		{
-			std::cout << *pos << std::endl;
+			config[ret[1]] = ret[2];
 		}
-		//res[0]存放匹配到的完整字符串
-		std::cout << "name:" << res[1] << std::endl;
-	}*/
+	}
+	Conf_File.close();
+	return 0;
+}
+
+int Regex_line(string RegBuf, string Regexp, char* ret)
+{
+	boost::regex reg(Regexp);
+	boost::regex::icase;
+	boost::cmatch res;
+
+	if (boost::regex_search(RegBuf.c_str(), res, reg))
+	{
+		strncpy_s(ret, 1024, res.str(0).c_str(), strlen(res.str(0).c_str()));
+		return 1;
+	}
+	return 0;
+}
+
+int Regex_file(string File_Name)
+{
+	ifstream Tmp_File;
+	string Rd_Buf;
+	int line = 0;
+	string Regexp;
+	char ret[1024];
+
+	Tmp_File.open(File_Name, ios::in);
+	if (!Tmp_File.is_open())
+	{
+		cerr << "[出错]--------------------------------------------打开检查文件：" << File_Name << endl;
+		return -1;
+	}
+	//cout << "读取文件" << File_Name << endl;
+	while (!Tmp_File.eof())
+	{
+		getline(Tmp_File, Rd_Buf);
+		line++;
+		//cout << "文件行数：" << line << endl;
+		for (map<string, string>::iterator iter = Pre_Conf.find("6_regex_rule1"); iter != Pre_Conf.end(); iter++)
+		{
+			//cout << "开始匹配规则" << iter->second << endl;
+			Regexp = iter->second;
+			if (Regex_line(Rd_Buf, Regexp, ret))
+			{
+				//cout << "匹配结果"<<ret << endl;
+				if (0 != Judge_Cmpfile_Is_Same(File_Name, line, Rd_Buf))
+				{
+					Output_Ret_To_Csv(File_Name, line, ret, Rd_Buf);	
+					//continue;
+				}
+				else
+				{
+					continue;
+				}
+				
+			}
+			continue;
+		}
+	}
+	cerr << "[成功]--------------------------------------------检查文件：" << File_Name << endl;
+	Tmp_File.close();
+
 	return 0;
 }
