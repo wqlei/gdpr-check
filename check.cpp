fde6c3f662ec8c1ea7411171609a56cf7eed5b6d
wqlei <40601230+wqlei@users.noreply.github.com>
2018-07-04 23:24:16 +0800

    Update check.cpp

--- check.cpp
+++ check.cpp
@@ -1,21 +1,49 @@
-#include<iostream>
+#include"regexp.h"
+#include"check_files.h"
+#include"outcsv.h"
 
 using namespace std;
 
+map<string, string> Pre_Conf;
+extern vector<string> SrcFiles;
+
 int main()
 {
-	char Input_Exp_Dir[100];
-	char Input_Src_Dir[100];
-	cerr<<"输入export文件夹和源文件夹地址："<<endl;
-	cin>>Input_Exp_Dir>>Input_Src_Dir;
-	if(!Files_Analys(Input_Exp_Dir, Input_Src_Dir))
+	//cerr << "[开始]--------------------------------------------开始读取配置" << endl;
+	if (Get_Conf(Pre_Conf)<0)
 	{
-		cerr<<"读取文件夹失败，请检查输入！"<<endl;
-		return 0;
+		cerr << "[出错]--------------------------------------------读取配置" << endl;
+		exit(-1);
 	}
-	
-	for()
+	else
 	{
-		
+		cerr << "[成功]--------------------------------------------读取配置" << endl;
 	}
+
+	/*cout << "读取的配置有：" << endl;
+	for (map<string, string>::iterator iter = Pre_Conf.begin(); iter != Pre_Conf.end(); iter++)
+		cout << iter->second << endl;*/
+
+	//cerr << "[开始]----------------------------------------读取检查文件" << endl;
+	if (Files_Analys(Pre_Conf["5_export_directory"], Pre_Conf["4_src_directory"])<0)
+	{
+		cerr << "[出错--------------------------------------------读取检查文件" << endl;
+		exit(-1);
+	}
+	else
+	{
+		cerr << "[成功]--------------------------------------------读取检查文件" << endl;
+	}
+
+	Creat_Out_Csv();
+	/*cout << "读取的文件有：" << endl;
+	for(vector<string>::iterator iter = SrcFiles.begin();iter!=SrcFiles.end();iter++)
+		cout << *iter << endl;*/
+	for (vector<string>::iterator iter = SrcFiles.begin(); iter != SrcFiles.end(); iter++)
+	{
+		//cout << "开始正则文件：" <<*iter<< endl;
+		Regex_file(*iter);
+	}
+	system("pause");
+	return 0;
 }
