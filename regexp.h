ea1e379b825dfa2707ff1b1dcf8bdf2a10eb28e1
wqlei <40601230+wqlei@users.noreply.github.com>
2018-07-10 20:07:21 +0800

    Update regexp.h

--- regexp.h
+++ regexp.h
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<boost/regex.hpp>
+#include<time.h>
 #include<string>
 #include<map>
 
 
-
+std::string getTime();
 int Get_Conf(std::map<std::string, std::string> &config);
 int Regex_line(std::string RegBuf, std::string Regexp, char* ret);
 int Regex_file(std::string File_Name);
